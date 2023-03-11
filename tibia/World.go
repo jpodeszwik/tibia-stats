814cdb4f451f067bec0b8f1a21649ba73d04be4f
Jakub Podeszwik <jakubpodeszwik@gmail.com>
2023-03-11 21:03:18 +0100

    Api to fetch Worlds

---
+++ tibia/World.go
@@ -0,0 +1,56 @@
+package tibia
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"tibia-exp-tracker/slices"
+)
+
+type WorldResponse struct {
+	Name string
+}
+
+type world struct {
+	Name string `json:"name"`
+}
+
+type worlds struct {
+	RegularWorlds []world `json:"regular_worlds"`
+}
+
+type worldsResponse struct {
+	Worlds worlds `json:"worlds"`
+}
+
+func (ac *ApiClient) FetchWorlds() ([]WorldResponse, error) {
+	url := fmt.Sprintf("%s/v3/worlds", ac.baseUrl)
+
+	resp, err := ac.httpClient.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	if resp.Body != nil {
+		defer func() {
+			err := resp.Body.Close()
+			if err != nil {
+				log.Printf("Failed to close body %s", err)
+			}
+		}()
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	data := worldsResponse{}
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return nil, err
+	}
+
+	return slices.MapSlice(data.Worlds.RegularWorlds, func(world world) WorldResponse {
+		return WorldResponse{Name: world.Name}
+	}), nil
+}
