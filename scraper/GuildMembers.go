59c9247e19073f9c2c1a6dddacccbb27bcd7bae3
Jakub Podeszwik <jakubpodeszwik@gmail.com>
2023-05-04 07:45:33 +0200

    Fetch initial memberships in parallel

--- scraper/GuildMembers.go
+++ scraper/GuildMembers.go
@@ -17,9 +17,15 @@ type GuildMembers struct {
 	m           sync.RWMutex
 }
 
+type workResult struct {
+	guildName string
+	members   []string
+	err       error
+}
+
 func (gm *GuildMembers) Start() {
 	ticker := time.NewTicker(guildMembersRefreshInterval)
-	err := gm.fetchGuildMembers()
+	err := gm.initialFetch(4)
 	if err != nil {
 		logger.Error.Fatal(err)
 	}
@@ -29,6 +35,76 @@ func (gm *GuildMembers) Start() {
 			gm.fetchGuildMembers()
 		}
 	}()
+}
+
+func (gm *GuildMembers) initialFetch(workers int) error {
+	start := time.Now()
+
+	var wg sync.WaitGroup
+
+	work := make(chan string, 100)
+	result := make(chan workResult, 100)
+	for i := 0; i < workers; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			for guildName := range work {
+				guild, err := retry(func() (*tibia.GuildResponse, error) {
+					return gm.api.FetchGuild(guildName)
+				}, 5)
+				if err != nil {
+					logger.Error.Printf("Failed to fetch guild %v %v", guildName, err)
+					result <- workResult{
+						guildName: guildName,
+						err:       err,
+					}
+				}
+				var members []string
+				for _, member := range guild.Members {
+					members = append(members, member.Name)
+				}
+				result <- workResult{
+					guildName: guildName,
+					members:   members,
+				}
+			}
+		}()
+	}
+
+	guilds := gm.guilds.getGuilds()
+	go func() {
+		for _, guildName := range guilds {
+			work <- guildName
+		}
+		close(work)
+		wg.Wait()
+		close(result)
+	}()
+
+	playerGuild := make(map[string]string)
+	var err error
+	for res := range result {
+		if res.err != nil {
+			err = res.err
+			continue
+		}
+
+		for _, member := range res.members {
+			playerGuild[member] = res.guildName
+		}
+	}
+	if err != nil {
+		return err
+	}
+
+	gm.m.Lock()
+	gm.playerGuild = playerGuild
+	gm.m.Unlock()
+	gm.handler(playerGuild)
+
+	logger.Info.Printf("Finished fetching %v guilds members with %v workers, %v memberships found in %v", len(guilds), workers, len(playerGuild), time.Since(start))
+	return nil
 }
 
 func (gm *GuildMembers) fetchGuildMembers() error {
